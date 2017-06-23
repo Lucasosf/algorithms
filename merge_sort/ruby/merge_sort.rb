e974ba7a9b1ee409b704ac377159305294f1ec27
Lucas Oliveira <loslucassilva@gmail.com>
2017-06-23 17:30:29 -0300

    Add implementation of the merge sort

--- merge_sort/ruby/merge_sort.rb
+++ merge_sort/ruby/merge_sort.rb
@@ -1,14 +1,27 @@
-# Mergesort(A):=end
-# 1. return A if n == 1
-# 2. left = left half of A
-# 3. right = right half of A
-# 4. sorted_left = Mergesort(left)
-# 5. sorted_right = Mergesort(right)
-# 6. return merge(sorted_left, sorted_right)
-
 class MergeSort
   def self.sort(list)
-    # TODO
-    list
+    return list if list.size == 1
+
+    left = list.take(list.size / 2)
+    right = list.drop(list.size / 2)
+
+    sorted_left = sort(left)
+    sorted_right = sort(right)
+
+    merge(sorted_left, sorted_right)
+  end
+
+  def self.merge(left, right)
+    sorted = []
+
+    until left.empty? || right.empty?
+      sorted << if left.first <= right.first
+                  left.shift
+      else
+        right.shift
+      end
+    end
+
+    sorted + left + right
   end
 end
