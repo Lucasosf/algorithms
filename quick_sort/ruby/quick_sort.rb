4844040d48b5ec7d180e1cf9c68c165c7788b8ab
Lucas Oliveira <loslucassilva@gmail.com>
2017-07-04 13:02:34 -0300

    Add quick sort algo implementation

--- quick_sort/ruby/quick_sort.rb
+++ quick_sort/ruby/quick_sort.rb
@@ -1,6 +1,20 @@
 class QuickSort
   def self.sort(list)
-    # TODO
-    list
+    return list if list.size <= 1
+
+    pivot = list[rand(list.size)]
+
+    left = []
+    right = []
+
+    list.each do |item|
+      if item > pivot
+        right.push(item)
+      else
+        left.push(item)
+      end
+    end
+
+    sort(left) + sort(right)
   end
 end
